04042e51ac48cfb0eabc363094f0133a6b7ce38f
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2020-05-03 17:05:18 -0600

    Update and rename gui.py to main.py

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import subprocess
-import sys
-import PySimpleGUI as sg
-
+import subprocess, sys, PySimpleGUI as sg
 
 def main():
     sg.theme('black')
 
     layout = [  [sg.Text('Enter a command to execute (e.g. dir or ls)')],
-                [sg.Input(key='_IN_')],             # input field where you'll type command
-                [sg.Output(size=(60,15))],          # an output area where all print output will go
-                [sg.Button('Run'), sg.Button('Exit')] ]     # a couple of buttons
+                [sg.Input(key='_IN_')], 
+                [sg.Output(size=(60,15))],  
+                [sg.Button('Run'), sg.Button('Exit')] ] 
 
     window = sg.Window('title', layout, no_titlebar=False, alpha_channel=.9, grab_anywhere=True,size=(512, 512),location=(666, 256))
 
-    while True:             # Event Loop
+    while True:      
         event, values = window.Read()
-        if event in (None, 'Exit'):         # checks if user wants to exit
+        if event in (None, 'Exit'):    
             break
 
-        if event == 'Run':                  # the two lines of code needed to get button and run command
+        if event == 'Run':                 
             runCommand(cmd=values['_IN_'], window=window)
 
     window.Close()
@@ -31,9 +28,9 @@ def runCommand(cmd, timeout=None, window=None):
         line = line.decode(errors='replace' if (sys.version_info) < (3, 5) else 'backslashreplace').rstrip()
         output += line
         print(line)
-        window.Refresh() if window else None        # yes, a 1-line if, so shoot me
+        window.Refresh() if window else None        
     retval = p.wait(timeout)
-    return (retval, output)                         # also return the output just for fun
+    return (retval, output)                  
 
 if __name__ == '__main__':
     main()
